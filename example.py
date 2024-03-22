3a59200ebd478706e1a21fe0ef19668b7bb090b7
arsul jawed <128362584+arsuljawed12@users.noreply.github.com>
2024-03-22 15:22:04 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,56 @@
+# year = int(input('enter the years:'))
+# if(year%400==0) and (year%100==0):
+
+#     print('The leap year is:',year)
+
+# elif(year%4==0) and (year%100!=0):
+
+#     print('The leap year is:',year)
+# else:
+
+#      print('The leap year is not:',year)
+
+# num = int(input('Enter the num:'))
+
+# for  i in range(2, num//2):
+#     if(num % i == 0):
+#       print('not prime')
+#       break
+# else:
+#    print("prime")
+
+# num = int(input('Enter  a number : '))
+# if num > 0:
+#     print("Positive number")
+
+# else:
+#     print("negative number")
+
+# def num(n):
+#     num = 5
+#     for i in range(0, n):
+#         for j in range(0, i+1):
+#             print(num, end=" ")
+#             num = num + 5
+#         print()
+
+# n = 5
+# num(n)
+
+
+# num = int(input("Enter the num:"))
+# for i in range(0,num):
+#         for j in range(0,i+1):
+#             print('*', end=" ")
+#         print()
+
+import turtle
+
+pen = turtle.Turtle()
+pen.fillcolor("red")
+pen.begin_fill()
+pen.circle(10)
+pen.end_fill()
+
+
+
